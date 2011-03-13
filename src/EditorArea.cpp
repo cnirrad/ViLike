33d1964b056030f8f7457f3535e78437f821612a
Darrin Collins <cnirrad@gmail.com>
2011-03-12 19:36:54 -0600

    Call show_all for an Editor added to the EditorArea.

--- src/EditorArea.cpp
+++ src/EditorArea.cpp
@@ -14,6 +14,6 @@ void EditorArea::add_editor(SourceEditor *editor)
 {
     m_editors[editor->get_filepath()] = editor;    
     int idx = append_page(*editor);
+    editor->show_all();
     set_current_page(idx);
-
 }
